0910c6ea10d92f56c7314feb503313250047d6e6
Samroz Javed <samroz.javed@gmail.com>
2022-07-26 12:45:25 +0200

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Samroz Javed on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
